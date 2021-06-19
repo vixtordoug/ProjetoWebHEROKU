e8261bece697a074dcab4564080c29674a5fd865
Víctor Douglas <victordouglas999@gmail.com>
2021-06-19 14:25:08 -0300

    create&read local

--- crud/create.php
+++ crud/create.php
@@ -1,25 +1,26 @@
 <?php
 //Página de Conexão com database
 //Dados de conexão local
-/* 
+
 $servername = "localhost";
 $username   = "root";
 $password   = "";
 $dbname     = "projetoweb";
- */
+
 
 //Dados de conexão InfinityFree
-$servername = "sql204.epizy.com";
+/* $servername = "sql204.epizy.com";
 $username   = "epiz_28925354";
 $password   = "pdWPy1MqQxMY";
 $dbname     = "epiz_28925354_projetoWeb";
+ */
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "INSERT INTO aluno (aluno, disciplina, nota1, nota2)
-  VALUES ('Víctor', 'LPW', '10.0', '10.0')";
+  VALUES ('Rafaela', 'ADS', '9.0', '10.0')";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "Cadastrado com sucesso!";
---
+++ crud/read.php
@@ -0,0 +1,53 @@
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Aluno</th><th>Disciplina</th><th>Nota I</th><th>Nota II</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+//Dados de conexão local
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "projetoweb";
+
+
+//Dados de conexão InfinityFree
+/* $servername = "sql204.epizy.com";
+$username   = "epiz_28925354";
+$password   = "pdWPy1MqQxMY";
+$dbname     = "epiz_28925354_projetoWeb"; */
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT id,aluno, disciplina, nota1, nota2 FROM aluno");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
